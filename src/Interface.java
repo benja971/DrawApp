cd182268e81a7fadf3460704d2e437a8ef7a08ec
benja971 <46868460+benja971@users.noreply.github.com>
2021-03-24 14:20:26 -0400

    Début création des figures avec  la sourie

--- src/Interface.java
+++ src/Interface.java
@@ -1,60 +1,99 @@
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-// 
+
 /**
  * Interface
  */
-public class Interface extends JFrame {
+public class Interface extends JFrame implements ActionListener {
 
-    private static final long serialVersionUID = 1L;
-    
+    private Dimension dimension;
+    private JPanel hotbar;
+    private JPanel menu;
+    private JMenuBar mb;
+    private JMenu file;
+    private JMenu home;
+    private JMenu display;
+    private JButton BPoint;
+    private JButton BSegmt;
+    private JButton BCircle;
+    private JButton BPolygon;
+    private JButton Color;
+    private Editeur ed1;
+    private JColorChooser colorsC;
+    private Container content;
+
     public Interface(String name) {
         setTitle(name);
-        Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        setLayout(new GridLayout(5, 0, 15, 15));
+        dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
         setSize((int)(dimension.getWidth()/2), (int)(dimension.getHeight()/2));
-        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        hotbar = new JPanel();
+        menu = new JPanel();
+    
+        mb = new JMenuBar(); 
+        file = new JMenu("File");
+        home = new JMenu("Home");
+        display = new JMenu("Display");
+    
+        BPoint = new JButton("Point");
+        BSegmt = new JButton("Segment");
+        BCircle = new JButton("Circle");
+        BPolygon = new JButton("Polygon");
+        Color = new JButton("Color");
+    
+        ed1 = new Editeur();
+    
+        colorsC = new JColorChooser();
+        
+        content = this.getContentPane();
+        
+        BPoint.addActionListener(this);
+        BSegmt.addActionListener(this);
+        BCircle.addActionListener(this);
+        BPolygon.addActionListener(this);
+    
+        hotbar.setLayout(new GridLayout(2, 1));
+        menu.setLayout(new GridLayout());
+    
+        mb.add(file);
+        mb.add(home);
+        mb.add(display);
+    
+        menu.add(BPoint);
+        menu.add(BSegmt);
+        menu.add(BCircle);
+        menu.add(BPolygon);
+        menu.add(Color);
+    
+        ed1.addMouseListener(ed1);
+    
+        hotbar.add(mb); 
+        hotbar.add(menu); 
+    
+        content.add(hotbar, BorderLayout.NORTH);
+        content.add(ed1, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
-        JFrame gui = new Interface("DrawApp");
-        JPanel menu = new JPanel();
-        JPanel tst = new JPanel();
-        JTabbedPane ctrlBar = new JTabbedPane();
-        Editeur ed1 = new Editeur();
-        JComponent fichier = new JMenu("fichier");
-        JComponent acceuil = new JMenu("acceuil");
-        JComponent affichage = new JMenu("affichage");
-        JColorChooser colorsC = new JColorChooser();
-
-        menu.setLayout(new GridLayout(1, 3));
-        menu.setBounds(0, 0, (int)gui.getSize().getWidth(), (int)gui.getSize().getHeight()/10);
-        menu.setBackground(Color.green);
-
-        tst.setBackground(Color.red);
-        tst.setBounds(0, 0, (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight()/10);
-        
-        menu.add(fichier); 
-        menu.add(acceuil);
-        menu.add(affichage);
-        gui.add(menu);
-        gui.add(tst);
-
-        // gui.add(ed1);
-
+        //Ne pas oublier de chnager les layout de base des jpannels
+        Interface gui = new Interface("DrawApp");
         gui.setVisible(true);
     }
 
+    public void actionPerformed(ActionEvent evt) {
+        JButton source = (JButton)evt.getSource();
+        ed1.setSelectedFigure(source.getText());
+    }
 }
